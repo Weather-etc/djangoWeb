7205afa886ead46d774ede9360084fb4992e15e2
Weather-Forecast <2263704336@qq.com>
2023-07-10 17:11:43 +0800

    Push forward a little...

--- djangoWeb/settings.py
+++ djangoWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-m!)-35e^7%p8d^_0-u&nul+dg7obvmkhykh#0po&yafr=3x^#z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'learn',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "djangoWeb.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "myWeb",
+        "user": "root",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "PORT": 3306,
     }
 }
 
